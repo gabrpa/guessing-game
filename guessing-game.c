771b9806e156959636a4b38b5df944ad6da95df9
gabrpa <gabrpa@icloud.com>
2024-12-01 17:43:35 -0300

    feat(added non number recognition + small adjustments)

--- guessing-game.c
+++ guessing-game.c
@@ -2,34 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-void randomF()
-{
-    srand(time(NULL));
-    int randomNumber = rand() % 11;
-    int pickedNumber;
+void randomF() {
+  int randomNumber = rand() % 11;
+  int pickedNumber;
 
-    while (1)
-    {
-        printf("Escolha um número de 1 a 10:\n");
-        scanf("%d", &pickedNumber);
-        if (pickedNumber == randomNumber)
-        {
-            printf("Parabéns você acertou!!\n");
-            break;
-        }
-        else
-        {
-            printf("Número incorreto. O numero secreto é %d. Tente novamente\n", randomNumber);
-            randomNumber = rand() % 11;
-        }
+  while (1) {
+    printf("Escolha um número de 0 a 10:\n");
+    if (scanf("%d", &pickedNumber) == 1 && pickedNumber >= 0 &&
+        pickedNumber <= 10) {
+      if (pickedNumber == randomNumber) {
+        printf("Parabéns você acertou!!\n");
+        break;
+      } else {
+        printf("Número incorreto. Tente novamente:\n");
+      }
+    } else {
+      printf("Valor incorreto. Por favor, escolha um número entre 0 e 10:\n");
+      while (getchar() != '\n')
+        ;
     }
+  }
 }
 
-int main()
-{
-
-    printf("**************************************\n");
-    printf("Bem vindo ao nosso jogo de adivinhação\n");
-    printf("**************************************\n");
-    randomF();
+int main() {
+  srand(time(NULL));
+  printf("**************************************\n");
+  printf("Bem vindo ao nosso jogo de adivinhação\n");
+  printf("**************************************\n");
+  randomF();
 }
